b2f5c989bdd14d242beacac6da901ea7b95138d2
lamaisondeprovence-gif <lamaisondeprovence@hotmail.com>
2026-06-04 12:15:57 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,23 +1,2 @@
-import { NextResponse } from 'next/server';
-
-const COOKIE_NAME = 'kairos_auth';
-const PASSWORD = 'TON_MOT_DE_PASSE';
-
-export function middleware(request) {
-  const { pathname } = request.nextUrl;
-
-  if (pathname.startsWith('/api/') || pathname.startsWith('/_next/') || pathname === '/login') {
-    return NextResponse.next();
-  }
-
-  const cookie = request.cookies.get(COOKIE_NAME);
-  if (cookie?.value === PASSWORD) {
-    return NextResponse.next();
-  }
-
-  return NextResponse.redirect(new URL('/login', request.url));
-}
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|icons|favicon.ico).*)'],
-};
+export function middleware() {}
+export const config = { matcher: [] };
